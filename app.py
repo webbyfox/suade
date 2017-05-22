260ea203bf96aacb29e490aa3f44b3a32c0193e5
Rizwan Mansuri <mr.mansuri@gmail.com>
2017-05-22 21:46:07 +0100

    Added reports models and api method to retrieve data

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask import jsonify
+
+from model import Reports
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'ppostgres://interview:LetMeIn@candidate.suade.org/suade'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://interview:LetMeIn@candidate.suade.org/suade'
 db = SQLAlchemy(app)
-
+db.init_app(app)
 
 @app.route("/")
 def hello():
     return "Welcome to Suade Reporting API!"
 
+
+
+
+@app.route('/api/reports/', methods=['GET'])
+def get_all_reports():
+    reports = Reports.query.all()
+    return jsonify([item.type for item in reports])
+
+@app.route('/api/report/<int:id>/', methods=['GET'])
+def get_report(id):
+    report = Reports.query.get(id)
+    return report.type
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This page does not exist', 404
 
 if __name__ == "__main__":
     app.run()
---
+++ model.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+from flask.ext.sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://interview:LetMeIn@candidate.suade.org/suade'
+db = SQLAlchemy(app)
+db.init_app(app)
+
+
+class Reports (db.Model):
+ 	id = db.Column(db.Integer, primary_key = True)
+ 	type =  db.Column(db.String(970))
